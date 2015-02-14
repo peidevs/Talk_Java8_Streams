1841b7a0d7b936d8c03761b4af6cd49b2eaf0af0
Michael Easter <codetojoy@gmail.com>
2015-02-14 12:57:46 -0400

    better commentary

--- slides/4_Java8BusinessLogic.java
+++ slides/4_Java8BusinessLogic.java
@@ -5,7 +5,12 @@ public class Java8BusinessLogic implements BusinessLogic {
     // Notes:
     // declarative vs imperative
     // specifies "what" without "how"
-    // new language features! lambda expressions, method references, default methods on interfaces
+    //     - we get optimizations (such as short-circuiting) for free
+    //
+    // new language features in their natural habitat: 
+    //     - lambda expression (see filter)
+    //     - method references (see map)
+    //     - default methods on interfaces (see sorted / Comparator.comparing() )
     
     public List<String> uglyReport(List<Donut> donuts, int priceThreshold, int numDonuts) {
         return donuts.stream()
